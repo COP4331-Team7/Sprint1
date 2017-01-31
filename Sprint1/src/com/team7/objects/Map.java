3bbf4742662766afeb8f8778d312150c6f4cca84
Or Oz <oroz77@gmail.com>
2017-01-31 15:59:28 -0500

    cleanup to allow creation of the first Map

--- Sprint1/src/com/team7/objects/Map.java
+++ Sprint1/src/com/team7/objects/Map.java
@@ -1,9 +1,6 @@
 package com.team7.objects;
 
-import com.team7.ProbabilityGenerator;
-import com.team7.objects.areaEffects.ElixirShower;
-import com.team7.objects.items.Obstacle;
-import com.team7.objects.resource.Elixir;
+
 import com.team7.objects.terrain.Desert;
 import com.team7.objects.terrain.FlatLand;
 import com.team7.objects.terrain.Hills;
@@ -15,35 +12,10 @@ public class Map {
     private Tile[][] tiles;
 
     public Map() {
-        tiles = new Tile[2][2];
-        tiles[0][0] = new Tile(new Desert());
-        tiles[0][1] = new Tile(new FlatLand());
-        tiles[1][0] = new Tile(new Hills());
-        tiles[1][1] = new Tile(new Mountains());
 
-        name = "Test Map, 2x2";
     }
 
-    public void setTileDetails
-            (double percentOfMapWithItems, double percentOfMapWithAreaEffects, double percentOfMapWithResources){
-        populateMap(percentOfMapWithItems, percentOfMapWithAreaEffects, percentOfMapWithResources);
-    }
 
-    private void populateMap
-            (double percentOfMapWithItems, double percentOfMapWithAreaEffects, double percentOfMapWithResources) {
-        for (Tile[] tileArray: tiles){
-            for (Tile tile: tileArray) {
-                if (ProbabilityGenerator.willOccur(percentOfMapWithItems)){
-                    tile.setItem(new Obstacle());
-                }
-                if (ProbabilityGenerator.willOccur(percentOfMapWithAreaEffects)){
-                    tile.setAreaEffect(new ElixirShower());
-                }
-                if (ProbabilityGenerator.willOccur(percentOfMapWithResources)){
-                    tile.setResource(new Elixir());
-                }
-            }
-        }
-    }
+
 
 }
