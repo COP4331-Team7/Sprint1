2f89e490c762ce3d280d9f172f476d00d078f152
Or Oz <oroz77@gmail.com>
2017-02-01 16:15:18 -0500

    fixed spelling of StructureCommandQueue

--- Sprint1/src/com/team7/objects/structure/StructureCommandQueue.java
+++ Sprint1/src/com/team7/objects/structure/StructureCommandQueue.java
@@ -2,7 +2,7 @@ package com.team7.objects.structure;
 
 import java.util.Queue;
 
-public class StructureCommadQueue {
+public class StructureCommandQueue {
     private int id;
     private Queue<String> commands;
 }
