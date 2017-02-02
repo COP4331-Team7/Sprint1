a323a2b27290e47ec2a915a444f456253e96ba30
Doug0855 <doug0855@gmail.com>
2017-02-01 22:09:09 -0500

    Army Tests

---
+++ Sprint1/src/com/team7/tests/ArmyTests.java
@@ -0,0 +1,35 @@
+package com.team7.tests;
+import com.team7.objects.Army;
+import com.team7.objects.Tile;
+import com.team7.objects.unit.Unit;
+import com.team7.objects.unit.combatUnit.MeleeUnit;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArmyTests {
+
+    @Test
+    public void testAddUnit() {
+        Tile t = new Tile();
+        Army a = new Army(t, 0);
+        MeleeUnit u = new MeleeUnit(t);
+        a.addUnitToArmy(u);
+
+        int armySize = a.getUnits().size();
+
+        assertEquals(armySize, 1);
+    }
+
+    @Test
+    public void testRemoveUnit() {
+        Tile t = new Tile();
+        Army a = new Army(t, 0);
+        MeleeUnit u = new MeleeUnit(t);
+        a.addUnitToArmy(u);
+        a.removeUnitFromArmy(u);
+        int armySize = a.getUnits().size();
+
+        assertEquals(armySize, 0);
+    }
+}
