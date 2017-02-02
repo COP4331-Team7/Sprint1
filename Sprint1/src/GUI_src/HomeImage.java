6bc4c3c0400cc965e17088ea91227b80b75e45a1
thomas1242 <thomaspalmer121@gmail.com>
2017-02-02 17:29:34 -0500

    move classes to individual files

---
+++ Sprint1/src/GUI_src/HomeImage.java
@@ -0,0 +1,36 @@
+package GUI_src;
+
+import com.team7.Main;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class HomeImage extends JPanel {
+
+        private BufferedImage image;
+        private Graphics2D g2d;
+
+        public HomeImage()
+        {
+            try {
+
+
+               image = ImageIO.read(new File(String.valueOf(Main.class.getClass().getResource("/resources/bg.jpg")).replace("file:","")));
+            }
+            catch (IOException e) {
+            }
+
+            Dimension size = new Dimension( image.getWidth(), image.getHeight());
+            setPreferredSize( size );
+        }
+
+        public void paintComponent( Graphics g )
+        {
+            super.paintComponent( g );
+            g.drawImage( image, 0, 0, this );
+        }
+}
---
+++ Sprint1/src/GUI_src/MainScreen.java
@@ -0,0 +1,52 @@
+package GUI_src;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MainScreen extends JPanel {
+
+    private ScreenSelectButtons screenSelectBtns = null;
+    private MainViewImage mainAreaView = null;
+    private MainViewInfo mainStatusInfo = null;
+    private Command commandSelecter = null;
+
+    public MainScreen() {
+
+        this.setLayout(new BorderLayout());
+
+        screenSelectBtns = new ScreenSelectButtons();
+        mainAreaView = new MainViewImage();
+        mainStatusInfo = new MainViewInfo();
+        commandSelecter = new Command();
+
+        this.add( screenSelectBtns, BorderLayout.NORTH );
+        this.add( mainAreaView, BorderLayout.CENTER );
+
+        JPanel temp = new JPanel(); // create JPanel to hold two other JPanels
+                                    // only one JPanel can be in a specific location, so we add 2 panels to a single panel
+                                    // then add the single panel where we want both panels
+
+        temp.add( mainStatusInfo, BorderLayout.WEST );
+        temp.add( commandSelecter, BorderLayout.SOUTH);
+
+        this.add( temp, BorderLayout.SOUTH );
+    }
+
+    public void giveCommandFocus() {          // give JPanel (commandSelecter) focus to 'make its KeyListener is active'
+        commandSelecter.setFocusable(true);
+        commandSelecter.requestFocus();
+    }
+
+    public ScreenSelectButtons getScreenSelectButtons() {
+            return screenSelectBtns;
+    }
+
+    public void drawMap() {
+        mainAreaView.drawMap();
+    }
+
+    public BufferedImage getMainViewImage() {
+        return mainAreaView.getCurrImage();
+    }
+}
