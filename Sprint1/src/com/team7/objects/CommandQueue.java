f1003405f9a57125206ce25f4972b6310485b31a
Or Oz <oroz77@gmail.com>
2017-02-02 12:10:14 -0500

    implementation of CommandQueue for push and remove

--- Sprint1/src/com/team7/objects/CommandQueue.java
+++ Sprint1/src/com/team7/objects/CommandQueue.java
@@ -4,4 +4,12 @@ import java.util.Queue;
 
 public class CommandQueue {
     private Queue<String> commands;
+    
+    private void addCommand(String command){
+        commands.add(command);
+    }
+
+    private String getNextCommand(){
+        return commands.remove();
+    }
 }
