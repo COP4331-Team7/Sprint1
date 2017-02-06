55215a8e24769c97e8eca53e50f2e055f442e793
acoverstone <acoverstone@ufl.edu>
2017-02-06 00:04:02 -0500

    :)

--- Sprint1/src/com/team7/tests/QueueTests.java
+++ Sprint1/src/com/team7/tests/QueueTests.java
@@ -4,6 +4,7 @@ import com.sun.source.tree.AssertTree;
 import com.team7.objects.*;
 import com.team7.objects.unit.Unit;
 import com.team7.objects.unit.combatUnit.MeleeUnit;
+import com.team7.objects.unit.nonCombatUnit.Colonist;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
@@ -12,6 +13,7 @@ import static org.testng.AssertJUnit.assertEquals;
 public class QueueTests {
 
     @Test
+    // test a queued army attack
     public void testArmyAttack() throws Exception {
 
         // start game
@@ -38,9 +40,43 @@ public class QueueTests {
 
         assertTrue(player2.getUnits().get(3).getUnitStats().getHealth() < 100);
 
+    }
 
+    @Test
+    // tests a queued order of healing units
+    public void testHealUnits() throws Exception {
+
+        // start game
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Game g = new Game(player1, player2);
+        g.startGame();
+
+        // Create army and put attack command in queue
+        Unit colonist = new Colonist(g.getMap().getGrid()[17][2], player1);
+        Unit melee = new MeleeUnit(g.getMap().getGrid()[17][2], player1);
+        player1.addUnit(melee);
+        player1.addUnit(colonist);
+
+
+        // Set melee health to low and create a base
+        melee.getUnitStats().setHealth(12);
+        ((Colonist) colonist).buildBase();
+
+        player1.getStructures().get(0).getCommandQueue().addCommand(new Command("heal/repair unit", null));
+        player1.getStructures().get(0).getCommandQueue().addCommand(new Command("heal/repair unit", null));
+
+        g.executeQueues();
+        g.executeQueues();
+
+        assertEquals(player1.getUnits().get(3).getUnitStats().getHealth(), 62);
 
     }
+
+
+
+
+
 
 
 }
